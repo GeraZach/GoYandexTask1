5ef9e26fcab89932334b7fef4f4c89f2ba317d6f
GeraZach <lopatkogeorgii@gmail.com>
2025-12-18 16:29:20 +0300

    new try

--- main.go
+++ main.go
@@ -130,7 +130,7 @@ func checkThresholds(load float64, totalMem, usedMem, totalDisk, usedDisk, total
 		networkUsage := float64(netUsage) / float64(totalNet)
 		if networkUsage > 0.9 {
 			freeMbits := (totalNet - netUsage) / (1024 * 1024)
-			fmt.Printf("Network bandwidth usage high: %.0f Mbit/s available\n", freeMbits)
+			fmt.Printf("Network bandwidth usage high: %.0v Mbit/s available\n", freeMbits)
 		}
 	}
 }
